57ac72aa65561dcacfe67e08869e044b40edcd17
Alex Crease <alexrcrease@gmail.com>
2023-08-21 15:18:52 +0100

    infra gh actions

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,7 +1,16 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+
 variable "aws_access_key" {
-    type = string
+  type = string
 }
 
 variable "aws_secret_key" {
-    type = string
+  type = string
 }
